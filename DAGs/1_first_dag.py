ba96b1c1c24dec6206e4880aa44a0a9f9a8960a1
Gaurav Singh <gauravbisht2803@gmail.com>
2026-04-23 11:55:17 +0530

    Add files via upload

---
+++ DAGs/1_first_dag.py
@@ -0,0 +1,27 @@
+from airflow.sdk import dag, task #It is important to import the task decorator from the sdk module, not from the main airflow module
+
+@dag(
+    dag_id ="first_dag",
+)
+def first_dag():#The dag decorator is used to define a DAG, and it takes the dag_id as an argument to uniquely identify the DAG
+    @task.python #The task decorator is used to define a task within the DAG, and it specifies that the task is a Python function
+    def first_task():
+        print("This is the first task of the first DAG!")
+
+    @task.python
+    def second_task():
+        print("This is the second task of the first DAG!")
+        
+    @task.python
+    def third_task():   
+        print("This is the third task of the first DAG!, DAG Complete")
+        
+    #Defining the task dependencies using the bitshift operator (>>). This means that first_task will run before second_task, and second_task will run before third_task.
+
+    first=first_task()
+    second=second_task()
+    third=third_task()
+    first >> second >> third
+
+#Registering the DAG    
+first_dag() #Finally, we call the function to create an instance of the DAG/to register the DAG with Airflow.
